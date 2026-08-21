f0856052cd30c4a24ef010560b2c5cc39ce6c8c2
Nishant-Kumar-Lakda <89572449+Nishant-Kumar-Lakda@users.noreply.github.com>
2026-08-21 23:40:58 +0530

    Add safe Android prototype build configuration

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "org.nova.os"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.nova.os"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+}
+
+dependencies {
+    testImplementation("junit:junit:4.13.2")
+}
